a5d1c88e18faebe4e2248ff623153aacdf212f8b
Retzam Tarle <Younggregs>
2021-03-01 21:24:27 +0100

    update database api connection

--- src/constants.js
+++ src/constants.js
@@ -7,7 +7,7 @@ export const URL = process.env.API_URL;
  */
 
 const API_ROOT_URL =
-	'http://localhost:8000/api';
+	'http://159.65.201.173:8000/api';
 const AUTH_URL = `${API_ROOT_URL}/auth/`;
 const LOGIN_URL = `${API_ROOT_URL}/login/`;
 
@@ -50,8 +50,8 @@ const DELETE_IMAGE_URL = `${API_ROOT_URL}/delete_image/`;
 const MISS_URL = `${API_ROOT_URL}/record_miss/`;
 const DELETE_MISS_URL = `${API_ROOT_URL}/delete_miss/`;
 
-const IMG_PATH_URL = 'http://127.0.0.1:8000';
-const ROOT_PATH_URL = 'http://127.0.0.1:8000';
+const IMG_PATH_URL = 'http://159.65.201.173:8000';
+const ROOT_PATH_URL = 'http://159.65.201.173/:3000';
 
 /*
     Other routes
